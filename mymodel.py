3d7fa9f1c7fed7f2cff261ce217ab13ee4943337
xxiang27 <57337158+xxiang27@users.noreply.github.com>
2022-03-16 02:12:38 -0400

    Add files via upload

--- mymodel.py
+++ mymodel.py
@@ -5,19 +5,19 @@ class Model(nn.Module):
     def __init__(self, dropout: float = 0.2) -> None:
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(12*128*128, 256, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(12*128*128, 256),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(256, 192, kernel_size=5, padding=2),
+            nn.MaxPool2d(kernel_size=3),
+            nn.Conv2d(256, 192, kernel_size=5),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=3),
+            nn.Conv2d(192, 384, kernel_size=3),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.Conv2d(384, 256, kernel_size=3),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.Conv2d(256, 256, kernel_size=3),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3),
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
